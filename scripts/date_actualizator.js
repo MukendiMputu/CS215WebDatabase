69a3636e251ec31877c0899e2ab6e6e23d1093b8
MukendiMputu <mputusam@live.com>
2020-02-17 19:10:26 -0600

    added querySelector
    Signed-off-by: MukendiMputu <mputusam@live.com>

--- scripts/date_actualizator.js
+++ scripts/date_actualizator.js
@@ -1,54 +1,70 @@
 
-var fullDate = new Date();
+var todaysDate = new Date();
 
-var shortMonths = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+const SHORTMONTHS = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
-var shortDays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+const SHORTDAYS = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
 
-var date = document.getElementById("date");
-date.innerHTML = fullDate.getDate();
+var dayOfWeek = document.querySelector("#day");
+var date = document.querySelector("#date");
+var month = document.querySelector("#month");
+var year = document.querySelector("#year");
 
-var month = document.getElementById("month");
-month.innerHTML = shortMonths[fullDate.getMonth()];
+var leftArrow = document.querySelector("#dp_left").addEventListener("click", backward, true);
 
-var year = document.getElementById("year");
-year.innerHTML = fullDate.getFullYear();
-
-adjustDate(date);
-
-var leftArrow = document.getElementById("dp_left").addEventListener("click", backward, true);
-
-var rightArrow = document.getElementById("dp_right").addEventListener("click", forward, true);
+var rightArrow = document.querySelector("#dp_right").addEventListener("click", forward, true);
 
 function backward(event) {
-    var dateElement = document.getElementById("date");
-    var actualDate = dateElement.innerHTML;
-    dateElement.innerHTML = (actualDate % 31) - 1;
-    adjustDate(date);
+    // Fetch the date in Milliseconds since Midnight Jan 1. 1970
+    var newDate = new Date(document.querySelector("#date").innerText, SHORTMONTHS.indexOf(document.querySelector("#month").innerText), document.querySelector("#date").innerText).valueOf() * -1;
+
+    var dayInMillisec = 1000 * 60 * 60 * 24;
+    newDate -= dayInMillisec ;
+
+    var d = new Date();
+    d.setTime(newDate);
+
+    adjustSup();
+    setDateElement(d);
 }
 
 function forward(event) {
-    var dateElement = document.getElementById("date");
-    var actualDate = dateElement.innerHTML;
-    dateElement.innerHTML = (parseInt(actualDate)  % 31) + 1;
-    adjustDate(date);
+    var newDate = new Date(document.querySelector("#date").innerText, SHORTMONTHS.indexOf(document.querySelector("#month").innerText), document.querySelector("#date").innerText).valueOf() * -1;
+
+    var dayInMillisec = 1000 * 60 * 60 * 24;
+    newDate += dayInMillisec ;
+
+    var d = new Date();
+    d.setTime(newDate);
+
+    adjustSup();
+    setDateElement(d);
 }
 
-function adjustDate(actDate) {
-    var date = actDate;
-
+function adjustSup() {
     if (date.innerHTML == 1 || date.innerHTML == 21 || date.innerHTML == 31) {
 
-        document.getElementById("ordinal").innerHTML = "st";
+        document.querySelector("#ordinal").innerHTML = "st";
 
     } else if (date.innerHTML == 2 || date.innerHTML == 22) {
 
-        document.getElementById("ordinal").innerHTML = "nd";
+        document.querySelector("#ordinal").innerHTML = "nd";
 
     } else if (date.innerHTML == 3 || date.innerHTML == 23) {
 
-        document.getElementById("ordinal").innerHTML = "rd";
+        document.querySelector("#ordinal").innerHTML = "rd";
     }else{
-        document.getElementById("ordinal").innerHTML = "th";
+        document.querySelector("#ordinal").innerHTML = "th";
     }
 }
+
+function setDateElement (dateToSet) {
+    var full_date = dateToSet
+    dayOfWeek.innerHTML = full_date == Date()? "Today " + SHORTDAYS[full_date.getDay()-1] + "," : SHORTDAYS[full_date.getDay()-1] + ",";
+    date.innerHTML = full_date.getDate();
+    month.innerHTML = SHORTMONTHS[full_date.getMonth()];
+    year.innerHTML = full_date.getFullYear();
+
+}
+setDateElement(todaysDate);
+adjustSup();
