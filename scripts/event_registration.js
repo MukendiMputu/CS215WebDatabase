78d3efe4c34d171f8c0746ca0f2fea95a78fd192
MukendiMputu <mputusam@live.com>
2020-02-22 23:38:19 -0600

    added some comments
    Signed-off-by: MukendiMputu <mputusam@live.com>

--- scripts/event_registration.js
+++ scripts/event_registration.js
@@ -1,13 +1,14 @@
 
+// Signup form event registration
 var myForm = document.querySelector("form").addEventListener("submit", Validate, false);
 
-// Selecting all the input elements
+// Selecting all the input elements of the signup form
 var nickname = document.forms["signup_form"]["nickname"];
 var email = document.forms["signup_form"]["email"];
 var password = document.forms["signup_form"]["loginPassword"];
 var password_confirm = document.forms["signup_form"]["loginPassword2"];
 
-// Registrating all the event listener
+// Registrating the event listener signup input elements
 nickname.addEventListener("keyup", verifyName, false);
 email.addEventListener("keyup", verifyEmail, false);
 password.addEventListener("keyup", verifyPwd, false);
